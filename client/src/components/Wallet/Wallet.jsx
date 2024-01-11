ef3f3f6380de8696cfdad88fb68ac12ab6f88d5e
akash-0233 <akashrajput0233@gmail.com>
2024-01-11 11:08:34 +0530

    A function for connection with metamask

--- client/src/components/Wallet/Wallet.jsx
+++ client/src/components/Wallet/Wallet.jsx
@@ -1,13 +1,27 @@
 import './Wallet.css';
+import ABI from "./ABI.json";
+import { useState } from 'react';
+import Web3 from "web3";
 
 const Wallet =()=>{
+
+      const init = async()=>{
+            try {
+                  const web3 = new Web3(window.ethereum);
+                  await window.ethereum.request({method:'eth_requestAccounts'});
+                  const contract = new web3.eth.Contract(ABI,"0x176aee6c6Cc353448a9A5cBEfE4D1a2E2d2DD9be");
+                  console.log(contract);
+            } catch (error) {
+                  alert("Install metaMask");
+            }
+      }
  
       return<>
       <div className="header">
       {false  && <button className="connectBTN">
          <a href="https://metamask.app.link/dapp/sriche.netlify.app/">Click For Mobile</a>
         </button>  } 
-       <button className="connectBTN">Connect Metamask</button>
+       <button className="connectBTN" onClick={init} >Connect Metamask</button>
       </div>
       </>
 }
